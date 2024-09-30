26a1d202efe6cbdf8e4f77629642c46007015e82
Jordy Devrix <2109jordy2241@gmail.com>
2024-09-30 17:43:21 +0200

    learned the difference between fgets and scanf

--- main.c
+++ main.c
@@ -25,16 +25,24 @@ int main(int argc, char *argv[]) {
     printf("The address of my_char in pointer is: %p\n", &my_char);
     printf("The address of my_char in dec is: %d\n", my_char);
 
-    double perimeter, area;
-    double width = atoi(argv[1]);
-    double height = atoi(argv[2]);
+    // double perimeter, area;
+    // double width = atoi(argv[1]);
+    // double height = atoi(argv[2]);
 
-    printf("The width is: %lf and the height is: %lf\n", width, height);
+    // printf("The width is: %lf and the height is: %lf\n", width, height);
     
-    perimeter = (width + height) * 2;
-    area = width * height;
+    // perimeter = (width + height) * 2;
+    // area = width * height;
     
-    printf("The perimeter is: %lf and the area is: %lf\n", perimeter, area);
+    // printf("The perimeter is: %lf and the area is: %lf\n", perimeter, area);
+
+    char mychar[20];
+    fgets(mychar, 20, stdin);
+    printf("The value of mychar is: %s\n", mychar);
+    for (int i = 0; i < strlen(mychar); i++) {
+        printf("The value of mychar[%d] is: %c\n", i, mychar[i]);
+    }
+
 
     return 0;
 } 
