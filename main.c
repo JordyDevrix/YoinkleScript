2885eb08c3ef22f327eab0dd2767dd62c20a59a7
Jordy Devrix <2109jordy2241@gmail.com>
2024-09-30 16:06:11 +0200

    Learned enums

--- main.c
+++ main.c
@@ -2,7 +2,12 @@
 #include "path_variable_scanner.h"
 
 int main(int argc, char *argv[]) {
-    path_variable_scanner(argc, argv);
+    // path_variable_scanner(argc, argv);
+    
+    enum prime_color {red, yellow, blue};
+    enum prime_color my_color, gregs_color;
+    my_color = three;
+    gregs_color = red;
     
     return 0;
 }
